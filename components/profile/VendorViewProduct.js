0a796d8999c3bf3431d94148b816e6d09e8ac561
chew0205 <50271734+chew0205@users.noreply.github.com>
2021-10-24 15:51:39 +0800

    VendorViewProduct without script and data

--- components/profile/VendorViewProduct.js
+++ components/profile/VendorViewProduct.js
@@ -1,45 +1,68 @@
-import { Row, Col, Image, Descriptions } from "antd"
+import { Row, Col, Image, Descriptions, Button } from "antd"
 import React from "react"
 import VendorProductCardList from "../vendor/VendorProductCardList"
 
 const VendorViewProduct = () => {
+
   return (
+      // size = size of data and iterate for each data
     // i'll fill this up with data later
-    <VendorProductCardList data={[]} />
-    // <Row gutter={24}>
-    //   <Col span={10}>
-    //     <Image
-    //       alt={"vendor profile picture"}
-    //       src="https://logos-world.net/wp-content/uploads/2020/12/Lays-Logo.png"
-    //       width={"100%"}
-    //     />
-    //   </Col>
-    //   <Col span={12}>
-    //     <Descriptions bordered>
-    //       <Descriptions.Item label="Name" span={3}>
-    //         get user name
-    //       </Descriptions.Item>
-    //       <Descriptions.Item label="Email" span={3}>
-    //         get user email
-    //       </Descriptions.Item>
-    //       <Descriptions.Item label="Description" span={3}>
-    //         get user email
-    //       </Descriptions.Item>
-    //       <Descriptions.Item label="Website Link" span={3}>
-    //         get website link
-    //       </Descriptions.Item>
-    //       <Descriptions.Item label="Social Media Links" span={3}>
-    //         get social media
-    //       </Descriptions.Item>
-    //       <Descriptions.Item label="Address" span={3}>
-    //         get user address
-    //       </Descriptions.Item>
-    //       <Descriptions.Item label="Category" span={3}>
-    //         get user category
-    //       </Descriptions.Item>
-    //     </Descriptions>
-    //   </Col>
-    // </Row>
+    //<VendorProductCardList data={[]} />
+      // for i=0 i<size; i++
+      //    <Col span={8}>
+      //    get image
+      //         <Image
+      //           alt={"vendor profile picture"}
+      //           src="https://logos-world.net/wp-content/uploads/2020/12/Lays-Logo.png"
+      //           width={"100%"}
+      //         />
+      //             <Button type={"primary"} block >
+      //                 get item name
+      //             </Button>
+    <Row gutter={[24, 24]}>
+      <Col span={8}>
+          <Image
+              alt={"vendor profile picture"}
+              src="https://logos-world.net/wp-content/uploads/2020/12/Lays-Logo.png"
+              width={"100%"}
+          />
+          <Button type={"primary"} block >
+              get item name
+          </Button>
+
+      </Col>
+      <Col span={8}>
+          <Image
+              alt={"vendor profile picture"}
+              src="https://logos-world.net/wp-content/uploads/2020/12/Lays-Logo.png"
+              width={"100%"}
+          />
+          <Button type={"primary"} block >
+              get item name
+          </Button>
+      </Col>
+        <Col span={8}>
+            <Image
+                alt={"vendor profile picture"}
+                src="https://logos-world.net/wp-content/uploads/2020/12/Lays-Logo.png"
+                width={"100%"}
+            />
+            <Button type={"primary"} block >
+                get item name
+            </Button>
+        </Col>
+
+        <Col span={8}>
+            <Image
+                alt={"vendor profile picture"}
+                src="https://logos-world.net/wp-content/uploads/2020/12/Lays-Logo.png"
+                width={"100%"}
+            />
+            <Button type={"primary"} block >
+                get item name
+            </Button>
+        </Col>
+    </Row>
   )
 }
 
